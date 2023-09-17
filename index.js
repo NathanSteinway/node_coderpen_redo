5add9af9dc842201e9b3bbeb7fe894abb0eddac1
Nathan Steinway <steinway.nathan@gmail.com>
2023-09-17 18:55:49 -0500

    issue with using the right methods

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require('fs');
   
     // EDIT: I swapped #1 and #2. When something needs to be sorted, I should really handle that first to avoid headaches I guess.
 
-    // 1) Each line should be ordered by it's timestamp.
+    // 1) Each line should be ordered by it's timestamp. - COMPLETE
     // 2) E, W, I, T, and so on should be displayed as their full names.
     // 3) If severity is over 15, display "Catastrophic"
 
@@ -46,9 +46,37 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
                 sortedDataArr.push(convertedData)
             }
         
-        // thank you based Bro Code @ Youtube
+        // Sort by timestamp!
         sortedDataArr.sort((a,b) => a[1] - b[1])
-        console.log(sortedDataArr)
+        
+            // Now that the array is sorted it's time to handle the <tag> names
+            // Loop through newly sorted array
+            for (var i = 0; i < sortedDataArr.length; i++) {
+
+                // grab the tag
+                let tag = sortedDataArr[i][0]
+
+                // conditional statement to handle the different cases
+                if (tag === "E") {
+                    fullName = tag.replace("E", "Error")
+                    tag = fullName
+                }
+                else if (tag === "W") {
+                    fullName=tag.replace("W", "Warn")
+                    tag = fullName
+                }
+                else if (tag === "I") {
+                    fullName = tag.replace("I", "Info")
+                    tag = fullName
+                }
+                else if (tag === "T") {
+                    fullName = tag.replace("T", "Trace")
+                    tag = fullName
+                }
+                else {
+                    // do nothing
+                }
+            }
     }
 
     catch (err) {
@@ -56,24 +84,3 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
         console.error('Uhhh woops', err);
     }
 })
-
-// Ok so this doesn't work bc it makes reaching the other information in the string with array methods annoying because the tag names are different lengths...
-
-// if (currentString.charAt(0) === ("E")) {
-//     let newString = currentString.replace("E", "Error");
-//     dataArr[currentString] = newString
-//     console.log(newString)
-// } 
-// else if (currentString.charAt(0) === ("W")) {
-//     let newString = currentString.replace("W", "Warn");
-//     dataArr[currentString] = newString
-//     console.log(newString)
-// }
-// else if (currentString.charAt(0) === ("I")) {
-//     let newString = currentString.replace("I", "Info")
-//     dataArr[currentString] = newString
-//     console.log(newString)
-// }
-// else {
-//     // If it ain't E, W, or I, don't do anything.
-// }
