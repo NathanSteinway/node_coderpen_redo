3932563a9b113c4ab37fd545576b74eeb39b91ef
Nathan Steinway <steinway.nathan@gmail.com>
2023-09-17 16:03:57 -0500

    adds instructions, comments, initial setup for solution

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// Recreation of my coderpen from Saturday given to me by Zeal IT Consultants.
+// By memory since I can't access the problem itself, so the assignment may not be 1 to 1.
+
 const fs = require('fs');
 
+// Format is <tag> <timestamp> <severity> <message>
+
+// To do:
+    // E, W, I, and so on should be displayed as their full names.
+    // Each line should be ordered by it's timestamp.
+    // If severity is over 15, display "Catastrophic"
+
+// Uses FileSystem node module to read data.txt then stuff it into data arg in it's callback function
 fs.readFile('data.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
@@ -7,7 +18,12 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
     }
 
     try {
-        console.log(data)
+
+        // Convert data to an array so I can use array methods on it!
+        const dataArr = data.split('\n');
+
+        // Sort to be easier to take in with the eyes
+        dataArr.sort();
     }
 
     catch (err) {
