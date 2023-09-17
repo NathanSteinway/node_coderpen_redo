d224ee926cc21a917e609ee868fe4f46aac51c32
Nathan Steinway <steinway.nathan@gmail.com>
2023-09-17 16:34:41 -0500

    removes unnecessary template code, rearranges data from txt file, adds conditional statements handling tag display

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const fs = require('fs');
 // Format is <tag> <timestamp> <severity> <message>
 
 // To do:
-    // E, W, I, and so on should be displayed as their full names.
-    // Each line should be ordered by it's timestamp.
-    // If severity is over 15, display "Catastrophic"
+    // 1) E, W, I, and so on should be displayed as their full names.
+    // 2) Each line should be ordered by it's timestamp.
+    // 3) If severity is over 15, display "Catastrophic"
 
 // Uses FileSystem node module to read data.txt then stuff it into data arg in it's callback function
 fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return console.log("Something went wrong!")
-    }
+
+    // removed if (err) statement from nodejs documentation, It doesn't seem necessary
 
     try {
 
@@ -24,9 +22,43 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
 
         // Sort to be easier to take in with the eyes
         dataArr.sort();
+
+        // Since data is now an array called dataArr, I can access the strings within each indice w/ a for loop!
+        for (var i = 0; i < dataArr.length; i++) {
+            // Pseudocode:
+                // Step 1:
+                    // The prefix for each log tag begins at the start of each string. They need to be replaced with their full names.
+                    // I need a way to track the current string, so I need a variable set to it.
+
+            let currentString = dataArr[i]
+
+                    // I need a method that will begin at the start of each string, then another to replace it w/ the full name without deleting the rest of the string.
+                    // My set of data has E, W, I, and invalid strings, so I need a conditional statement to treat them differently.
+            
+            if (currentString.charAt(0) === ("E")) {
+                let newString = currentString.replace("E", "Error");
+                dataArr[currentString] = newString
+                console.log(newString)
+            } 
+            else if (currentString.charAt(0) === ("W")) {
+                let newString = currentString.replace("W", "Warn");
+                dataArr[currentString] = newString
+                console.log(newString)
+            }
+            else if (currentString.charAt(0) === ("I")) {
+                let newString = currentString.replace("I", "Info")
+                dataArr[currentString] = newString
+                console.log(newString)
+            }
+            else {
+                // If it ain't E, W, or I, don't do anything.
+            }
+
+        }
     }
 
     catch (err) {
-        console.error('Error parsing JSON:', err);
+        // not working w/ json so replaced msg here to avoid confusing myself
+        console.error('Uhhh woops', err);
     }
 })
