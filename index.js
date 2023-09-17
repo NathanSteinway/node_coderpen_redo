14d21fe8fb03f0948cf92ed6350bacf71fd5b806
Nathan Steinway <steinway.nathan@gmail.com>
2023-09-17 16:46:21 -0500

    begins new approach

--- index.js
+++ index.js
@@ -18,43 +18,22 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
     try {
 
         // Convert data to an array so I can use array methods on it!
+        // Uses .split('\n') to split 'em up by line
         const dataArr = data.split('\n');
 
-        // Sort to be easier to take in with the eyes
-        dataArr.sort();
+            // My set of data has E, W, I, and invalid strings. I'd like to use a conditional statement to handle them, but I ran into a problem.
+            // If I use an conditional statement to display the full tag names then it makes grabbing the other info annoying because the tag names are different sizes.
+            // In order to avoid dataArr[i] index length shenanigans I should split the info into bits and pieces to be handled separately (maybe)
 
-        // Since data is now an array called dataArr, I can access the strings within each indice w/ a for loop!
-        for (var i = 0; i < dataArr.length; i++) {
-            // Pseudocode:
-                // Step 1:
-                    // The prefix for each log tag begins at the start of each string. They need to be replaced with their full names.
-                    // I need a way to track the current string, so I need a variable set to it.
-
-            let currentString = dataArr[i]
-
-                    // I need a method that will begin at the start of each string, then another to replace it w/ the full name without deleting the rest of the string.
-                    // My set of data has E, W, I, and invalid strings, so I need a conditional statement to treat them differently.
-            
-            if (currentString.charAt(0) === ("E")) {
-                let newString = currentString.replace("E", "Error");
-                dataArr[currentString] = newString
-                console.log(newString)
-            } 
-            else if (currentString.charAt(0) === ("W")) {
-                let newString = currentString.replace("W", "Warn");
-                dataArr[currentString] = newString
-                console.log(newString)
-            }
-            else if (currentString.charAt(0) === ("I")) {
-                let newString = currentString.replace("I", "Info")
-                dataArr[currentString] = newString
-                console.log(newString)
-            }
-            else {
-                // If it ain't E, W, or I, don't do anything.
-            }
-
+        // loops through dataArr
+        for (let i = 0; i < dataArr.length; i++) {
+        
+            // logs the split dataArr[i] in console
+            console.log(dataArr[i].split(' '))
         }
+        
+
+
     }
 
     catch (err) {
@@ -62,3 +41,24 @@ fs.readFile('data.txt', 'utf8', (err, data) => {
         console.error('Uhhh woops', err);
     }
 })
+
+// Ok so this doesn't work bc it makes reaching the other information in the string with array methods annoying because the tag names are different lengths...
+
+// if (currentString.charAt(0) === ("E")) {
+//     let newString = currentString.replace("E", "Error");
+//     dataArr[currentString] = newString
+//     console.log(newString)
+// } 
+// else if (currentString.charAt(0) === ("W")) {
+//     let newString = currentString.replace("W", "Warn");
+//     dataArr[currentString] = newString
+//     console.log(newString)
+// }
+// else if (currentString.charAt(0) === ("I")) {
+//     let newString = currentString.replace("I", "Info")
+//     dataArr[currentString] = newString
+//     console.log(newString)
+// }
+// else {
+//     // If it ain't E, W, or I, don't do anything.
+// }
